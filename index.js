f43ae77c024e3e6bc99cb1b4c825172a0c415748
taufiq72 <taufiqq1207@gmail.com>
2026-07-20 11:19:34 +0700

    menambahkan app.put

--- index.js
+++ index.js
@@ -40,3 +40,18 @@ app.get('/komik', async (req, res) => {
         res.send(error);
     }
 });
+
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.update(data);
+        res.send ({ message: 'komik berhasil diupdate', komik });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
